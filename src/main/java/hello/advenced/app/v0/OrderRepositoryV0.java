695f0c770a4d01f787958a82006303371f1283dd
jm <gssjmo@gmail.com>
2022-02-02 20:39:43 +0900

    프로젝트 생성하기

---
+++ src/main/java/hello/advenced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,21 @@
+package hello.advenced.app.v0;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+    public void save(String itemId) {
+        //저장 로직
+        if (itemId.equals("ex")) { // 들어오면 예외발생
+            throw new IllegalStateException("예외 발생!");
+        }
+        sleep(1000);
+    }
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/hello/advenced/app/v0/OrderServiceV0.java
@@ -0,0 +1,15 @@
+package hello.advenced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import hello.advenced.app.v0.OrderRepositoryV0;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV0 {
+    private final OrderRepositoryV0 orderRepository;
+    public void orderItem(String itemId) {
+        orderRepository.save(itemId);
+    }
+}
