d9b3a5eb88ba7882b6635333a69b8a5ab2d78486
jm <gssjmo@gmail.com>
2022-02-03 23:06:44 +0900

    로그추적기

--- src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -1,4 +1,4 @@
-package hello.advenced.app.v0;
+package hello.advanced.app.v0;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 @Repository
--- src/main/java/hello/advanced/app/v0/OrderServiceV0.java
+++ src/main/java/hello/advanced/app/v0/OrderServiceV0.java
@@ -1,7 +1,5 @@
-package hello.advenced.app.v0;
+package hello.advanced.app.v0;
 
-import lombok.RequiredArgsConstructor;
-import hello.advenced.app.v0.OrderRepositoryV0;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
